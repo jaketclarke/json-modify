b3d722fd101a73a4a6f7c6341f326d98b3408a72
Jake Clarke <jake@theredfox.group>
2020-11-30 16:32:22 +1100

    load json

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import os
 
 # print(data)
 
+def load_json(file):
+    with open(file) as file:
+        return json.load(file)
+
 directory = r'test-data-input'
 
 for subdir, dirs, files in os.walk(directory):
@@ -13,4 +17,6 @@ for subdir, dirs, files in os.walk(directory):
         filepath = subdir + os.sep + filename
 
         if filepath.endswith(".json"):
-            print (filepath)
+            # print (filepath)
+            data = load_json(filepath)
+            print(data)
