f6ff44fd0237c25fa09bc6c1cb24530aa65dd643
Jake Clarke <jake@theredfox.group>
2020-11-30 16:27:23 +1100

    loop and print parent dir name

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import json
+import os
 
-with open('test-data-input/dir-one/one.json') as file:
-    data = json.load(file)
+# with open('test-data-input/dir-one/one.json') as file:
+#     data = json.load(file)
 
-print(data)
+# print(data)
+
+directory = r'test-data-input'
+for entry in os.scandir(directory):
+    print(entry.name)
