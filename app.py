4e56725ee8b6c8752a0654d8d5a35d64e26c7378
Jake Clarke <jake@theredfox.group>
2020-11-30 16:32:30 +1100

    load json

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import json
 import os
 
-# with open('test-data-input/dir-one/one.json') as file:
-#     data = json.load(file)
-
-# print(data)
-
 def load_json(file):
     with open(file) as file:
         return json.load(file)
