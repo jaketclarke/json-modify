300c39a8c4d415ac129517f3a14aef4eadb096d0
Jake Clarke <jake.t.clarke@gmail.com>
2020-12-01 17:07:31 +1100

    add more logging, add a test for directorytree creation

--- tests.py
+++ tests.py
@@ -1,10 +1,23 @@
 from functions import *
 import pytest
+import os
 
+# tests for strip_parent_directory
 def test_strip_parent_dir():
-    result = strip_parent_directory('one/two/three')
-    assert result == 'two/three'
-
+    input='one/two/three'
+    output='two/three'
+    result = strip_parent_directory(input)
+    assert result == output, f"Expected {output}, Got {result}"
+    
 def test_strip_parent_dir_one_level():
+    input='one'
+    output=None
     result = strip_parent_directory('one')
-    assert result is None
+    assert result == output, f"Expected {output}, Got {result}"
+    
+# test makedir
+def test_make_directorytree_if_not_exists():
+    path = 'test1/test2'
+    make_directorytree_if_not_exists(path)
+    assert os.path.exists(path), f"Failed to create path {path}"
+    
