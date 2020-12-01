42c4428a5310aab9c33aacc19448ba16152a6d47
Jake Clarke <jake.t.clarke@gmail.com>
2020-12-01 17:18:25 +1100

    add test for outputting data

--- tests.py
+++ tests.py
@@ -27,3 +27,17 @@ def test_load_json():
     result = load_json(input)
     expected = {"id":4,"name":"Patricia Lebsack","username":"Karianne","email":"Julianne.OConner@kory.org","address":{"street":"Hoeger Mall","suite":"Apt. 692","city":"South Elvis","zipcode":"53919-4257","geo":{"lat":"29.4572","lng":"-164.2990"}},"phone":"493-170-9623 x156","website":"kale.biz","company":{"name":"Robel-Corkery","catchPhrase":"Multi-tiered zero tolerance productivity","bs":"transition cutting-edge web services"}}
     assert result == expected, f"Expected {expected}, Got {result}"
+
+# test write json
+def test_dump_json():
+    # set up output
+    outputfile = f'output{os.sep}dir-one{os.sep}four.json'
+    outputfolder = f'output{os.sep}dir-one'
+    input = {"id":4,"name":"Patricia Lebsack","username":"Karianne","email":"Julianne.OConner@kory.org","address":{"street":"Hoeger Mall","suite":"Apt. 692","city":"South Elvis","zipcode":"53919-4257","geo":{"lat":"29.4572","lng":"-164.2990"}},"phone":"493-170-9623 x156","website":"kale.biz","company":{"name":"Robel-Corkery","catchPhrase":"Multi-tiered zero tolerance productivity","bs":"transition cutting-edge web services"}}
+    # ensure dir exists
+    make_directorytree_if_not_exists(outputfolder)
+    # dump to disk
+    dump_json(input,outputfile)
+    # read from disk and check the result is the same
+    result = load_json(outputfile)
+    assert result == input, f"Expected {input}, Got {result}"
