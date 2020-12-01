ed0522ecd28c765bd903f0efc2c6a995e0953ed4
Jake Clarke <jake.t.clarke@gmail.com>
2020-12-01 12:31:26 +1100

    update logic

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ def load_json(file):
         return json.load(file)
 
 directory = r'test-data-input'
+key = 'foo'
+value = 'bar'
 
 for subdir, dirs, files in os.walk(directory):
     for filename in files:
         filepath = subdir + os.sep + filename
 
         if filepath.endswith(".json"):
-            # print (filepath)
+            
             data = load_json(filepath)
+            
+            data[key] = value
             print(data)
